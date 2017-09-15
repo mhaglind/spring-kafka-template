dbb71025f38f45bbd506f5700e2d5dbdfb8617fb
Mårten Haglind <marten@haglind.com>
2017-09-15 22:39:12 +0200

    First commit

---
+++ src/main/java/com/haglind/springkafkatemplate/SpringKafkaTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.haglind.springkafkatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaTemplateApplication.class, args);
+	}
+}
